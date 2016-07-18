6bf8b8130a2992760f2bcd5c2950ba7e44882f7d
Evan Hemsley <evan.hemsley@gmail.com>
2016-07-18 15:50:14 -0700

    code generator now outputs ugly code which is prettified by js-beautify library

--- src/transcript.js
+++ src/transcript.js
@@ -1,12 +1,14 @@
 const Lexer = require('./lexer.js');
 const Parser = require('./parser.js');
 const Codegen = require('./codegen.js');
+const Beautify = require('js-beautify').js_beautify;
 
 module.exports = {
   compile: function(string) {
     var tokens = Lexer.tokenize(string);
     var ast = Parser.parse(tokens);
     var code = Codegen.generate(ast);
-    return code + "\n";
+    var pretty_code = Beautify(code, { indent_size: 2 });
+    return pretty_code + "\n";
   }
 }
